0219fdb2765b211366299397adaef003cad0bd7e
Airezigma <gabs.sfv@gmail.com>
2021-03-05 19:03:13 -0300

    musica

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Musica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+
+            Console.WriteLine ("pegasus fantasy");
+            Console.ResetColor();
+            Console.WriteLine ("Faça elevar o cosmo no seu coração");
+            Console.WriteLine ("Todo o mal combater");
+            Console.WriteLine("Despertar o poder");
+            Console.WriteLine("Sua constelação sempre irá te proteger");
+            Console.WriteLine();
+            Console.WriteLine("Supera a dor e dá forças pra lutar");
+            Console.WriteLine("Pegasus Fantasy desejos a realizar");
+            Console.WriteLine("Pois as asas de um coração sonhador");
+            Console.WriteLine("Ninguém irá roubar");
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine("Saint Seiya");
+            Console.WriteLine("Guerreiros das Estrelas");
+            Console.WriteLine("Saint Seiya");
+            Console.WriteLine("Nada a temer oh yeah!");
+            Console.WriteLine("Saint Seiya");
+            Console.WriteLine("Unidos por sua força");
+            Console.WriteLine("Saint Seiya");
+            Console.WriteLine("Pegasus até vencer");
+            Console.ResetColor();
+            
+        }
+    }
+}
